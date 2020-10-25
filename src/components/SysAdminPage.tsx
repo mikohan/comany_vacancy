2cb212c881be62e2cabfaa37482f84b910eb4e36
mikohan <angara99@gmail.com>
2020-10-25 17:27:47 +0300

    Tested neovim

--- src/components/SysAdminPage.tsx
+++ src/components/SysAdminPage.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { durationPage } from '../config';
+import React from "react";
+import { motion } from "framer-motion";
+import { durationPage } from "../config";
+import Typography from "@material-ui/core/Typography";
 
 function SysAdminPage() {
+  const someConst: string = "45";
+  console.log(someConst);
   return (
     <motion.div
       exit={{ opacity: 0 }}
@@ -11,6 +14,8 @@ function SysAdminPage() {
       transition={{ duration: durationPage }}
     >
       <h1>SysAdmin Page goes Here</h1>
+
+      <Typography>Some stuff goes here</Typography>
     </motion.div>
   );
 }
