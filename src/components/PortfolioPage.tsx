09edafbb8eb92e5ba090869d4792f465e004640f
mikohan <angara99@gmail.com>
2021-01-08 17:21:17 +0300

    Added classes to portfolio page

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { durationPage } from '../config';
 import ReactPlayer from 'react-player';
+import { Grid, Paper } from '@material-ui/core';
+import { useStyles } from '../styles/HomePageStyles';
 
 function BackEndPage() {
+  const classes = useStyles();
   return (
     <motion.div
       exit={{ opacity: 0 }}
@@ -12,13 +15,18 @@ function BackEndPage() {
       transition={{ duration: durationPage }}
     >
       <h1>Portfolio Page</h1>
-
-      <ReactPlayer
-        url="https://youtu.be/IHpfcNRXUcU"
-        controls
-        width="100%"
-        height="100%"
-      />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.videoItem}>
+            <ReactPlayer
+              url="https://youtu.be/IHpfcNRXUcU"
+              controls
+              width="100%"
+              height="100%"
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </motion.div>
   );
 }
