fc1fae3f35684ef9cf5176c5f01ca18a2ab14278
mikohan <angara99@gmail.com>
2021-01-09 15:43:09 +0300

    Single component refactored

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -1,21 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { durationPage } from '../config';
-import ReactPlayer from 'react-player';
-import { Grid, Paper, Typography, Box } from '@material-ui/core';
-import { useStyles } from '../styles/PorfolioPageStyles';
-import { translateText } from '../translate/texts';
-import { ThemeContext } from '../context/ThemeContext';
+import { Grid } from '@material-ui/core';
 import '../tmp.scss';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import Button from '@material-ui/core/Button';
 import PortfolioSingle from './PortfolioSingle';
 
 function BackEndPage() {
-  const classes = useStyles();
-  const context = useContext(ThemeContext);
-  let { language } = context;
-  const { portfolio } = translateText[language];
   return (
     <motion.div
       exit={{ opacity: 0 }}
