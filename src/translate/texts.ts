0d8ab82b8f6f54acfccf4f18f2bf46eff9b8bf0c
mikohan <angara99@gmail.com>
2021-01-09 13:26:21 +0300

    Trnaslation working

--- src/translate/texts.ts
+++ src/translate/texts.ts
@@ -1,39 +1,131 @@
 interface TransText {
-	[key: string]: { [key: string]: string };
+  [key: string]: {
+    [key: string]: string | { [key: string]: { [key: string]: string } };
+  };
 }
 
 export const translateText: TransText = {
-	english: {
-		myName: 'My name is',
-		name: 'Vladimir Vostrikov',
-		mainHeading:
-			"I'm Full Stack developer with huge knowledge of Online Marketing, and Experience in ecommerce business.",
-		learnMore: 'Learn more',
-		blog_latestNews: 'Latest News',
-		blog_search: 'Search',
-		blog_noSearchResults: 'No Search Results',
-		blog_categories: 'Categories',
-	},
-	russian: {
-		myName: 'Меня зовут',
-		name: 'Владимир Востриков',
-		mainHeading:
-			'Я разработчик Full Stack с огромными знаниями в области интернет-маркетинга и опытом работы в сфере электронной коммерции.',
-		learnMore: 'Узнать больше',
-		blog_latestNews: 'Последние посты',
-		blog_search: 'Поиск',
-		blog_noSearchResults: 'Поиск не дал результатов',
-		blog_categories: 'Категории',
-	},
-	czech: {
-		myName: 'Jmenuji se ',
-		name: 'Vladimir Vostrikov',
-		mainHeading:
-			'Jsem vývojář Full Stack s obrovskými znalostmi online marketingu a zkušeností v oblasti elektronického obchodování.',
-		learnMore: 'Dozvědět se více',
-		blog_latestNews: 'Poslední zprávy',
-		blog_search: 'Vyhledávání',
-		blog_noSearchResults: 'Žádné výsledky vyhledávání',
-		blog_categories: 'Kategorie',
-	},
+  english: {
+    myName: 'My name is',
+    name: 'Vladimir Vostrikov',
+    mainHeading:
+      "I'm Full Stack developer with huge knowledge of Online Marketing, and Experience in ecommerce business.",
+    learnMore: 'Learn more',
+    blog_latestNews: 'Latest News',
+    blog_search: 'Search',
+    blog_noSearchResults: 'No Search Results',
+    blog_categories: 'Categories',
+    portfolio: {
+      venezo: {
+        title: `Venezo Project E-commerce project (Python, Django)`,
+        mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
+                    vitae nemo, exercitationem tenetur assumenda hic. Pariatur
+                    harum doloribus quas, modi porro voluptate dignissimos eaque
+                    exercitationem? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Nobis consequatur deleniti sunt beatae
+                    placeat dolorem fugit, aut voluptatum architecto mollitia
+                    cumque maxime veritatis alias quo a voluptates eaque
+                    officiis recusandae? Dolorem mollitia incidunt voluptas ut
+                    illum cum quisquam ex, perferendis, dolore ratione nulla
+                    quidem neque expedita! Eligendi obcaecati incidunt, ipsa ut
+                    labore, in minus quia, dolores earum voluptas magnam
+                    similique. Ipsam expedita laboriosam culpa? Ipsum, porro
+                    tempore illo aut tenetur eaque culpa quasi iusto blanditiis
+                    cumque cupiditate incidunt excepturi ducimus soluta
+                    reprehenderit ipsa molestiae ex ad corporis ratione repellat
+                    vel. Dolore perspiciatis quisquam aspernatur, cupiditate
+                    veritatis, placeat fuga omnis pariatur blanditiis iste quis,
+                    recusandae enim neque aliquid deserunt commodi nam doloribus
+                    odio itaque. Pariatur esse, accusantium quia explicabo sed
+                    perferendis! Vero molestiae exercitationem, odit voluptate
+                    molestias expedita facere modi eos blanditiis quia provident
+                    nihil, error nobis voluptatem ipsa iste saepe, obcaecati
+                    quidem itaque incidunt quos dolores! Debitis voluptas dolore
+                    repudiandae.`,
+      },
+    },
+  },
+  russian: {
+    myName: 'Меня зовут',
+    name: 'Владимир Востриков',
+    mainHeading:
+      'Я разработчик Full Stack с огромными знаниями в области интернет-маркетинга и опытом работы в сфере электронной коммерции.',
+    learnMore: 'Узнать больше',
+    blog_latestNews: 'Последние посты',
+    blog_search: 'Поиск',
+    blog_noSearchResults: 'Поиск не дал результатов',
+    blog_categories: 'Категории',
+    portfolio: {
+      venezo: {
+        title: `Venezo Project E-commerce project (Python, Django)`,
+        mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
+                    vitae nemo, exercitationem tenetur assumenda hic. Pariatur
+                    harum doloribus quas, modi porro voluptate dignissimos eaque
+                    exercitationem? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Nobis consequatur deleniti sunt beatae
+                    placeat dolorem fugit, aut voluptatum architecto mollitia
+                    cumque maxime veritatis alias quo a voluptates eaque
+                    officiis recusandae? Dolorem mollitia incidunt voluptas ut
+                    illum cum quisquam ex, perferendis, dolore ratione nulla
+                    quidem neque expedita! Eligendi obcaecati incidunt, ipsa ut
+                    labore, in minus quia, dolores earum voluptas magnam
+                    similique. Ipsam expedita laboriosam culpa? Ipsum, porro
+                    tempore illo aut tenetur eaque culpa quasi iusto blanditiis
+                    cumque cupiditate incidunt excepturi ducimus soluta
+                    reprehenderit ipsa molestiae ex ad corporis ratione repellat
+                    vel. Dolore perspiciatis quisquam aspernatur, cupiditate
+                    veritatis, placeat fuga omnis pariatur blanditiis iste quis,
+                    recusandae enim neque aliquid deserunt commodi nam doloribus
+                    odio itaque. Pariatur esse, accusantium quia explicabo sed
+                    perferendis! Vero molestiae exercitationem, odit voluptate
+                    molestias expedita facere modi eos blanditiis quia provident
+                    nihil, error nobis voluptatem ipsa iste saepe, obcaecati
+                    quidem itaque incidunt quos dolores! Debitis voluptas dolore
+                    repudiandae.`,
+      },
+    },
+  },
+  czech: {
+    myName: 'Jmenuji se ',
+    name: 'Vladimir Vostrikov',
+    mainHeading:
+      'Jsem vývojář Full Stack s obrovskými znalostmi online marketingu a zkušeností v oblasti elektronického obchodování.',
+    learnMore: 'Dozvědět se více',
+    blog_latestNews: 'Poslední zprávy',
+    blog_search: 'Vyhledávání',
+    blog_noSearchResults: 'Žádné výsledky vyhledávání',
+    blog_categories: 'Kategorie',
+    portfolio: {
+      venezo: {
+        title: `Venezo Project E-commerce project (Python, Django)`,
+        mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
+                    vitae nemo, exercitationem tenetur assumenda hic. Pariatur
+                    harum doloribus quas, modi porro voluptate dignissimos eaque
+                    exercitationem? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Nobis consequatur deleniti sunt beatae
+                    placeat dolorem fugit, aut voluptatum architecto mollitia
+                    cumque maxime veritatis alias quo a voluptates eaque
+                    officiis recusandae? Dolorem mollitia incidunt voluptas ut
+                    illum cum quisquam ex, perferendis, dolore ratione nulla
+                    quidem neque expedita! Eligendi obcaecati incidunt, ipsa ut
+                    labore, in minus quia, dolores earum voluptas magnam
+                    similique. Ipsam expedita laboriosam culpa? Ipsum, porro
+                    tempore illo aut tenetur eaque culpa quasi iusto blanditiis
+                    cumque cupiditate incidunt excepturi ducimus soluta
+                    reprehenderit ipsa molestiae ex ad corporis ratione repellat
+                    vel. Dolore perspiciatis quisquam aspernatur, cupiditate
+                    veritatis, placeat fuga omnis pariatur blanditiis iste quis,
+                    recusandae enim neque aliquid deserunt commodi nam doloribus
+                    odio itaque. Pariatur esse, accusantium quia explicabo sed
+                    perferendis! Vero molestiae exercitationem, odit voluptate
+                    molestias expedita facere modi eos blanditiis quia provident
+                    nihil, error nobis voluptatem ipsa iste saepe, obcaecati
+                    quidem itaque incidunt quos dolores! Debitis voluptas dolore
+                    repudiandae.`,
+      },
+    },
+  },
 };
