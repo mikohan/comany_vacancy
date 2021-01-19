0301dd618e79b180b08575fa5d06ab3c0087434d
mikohan <angara99@gmail.com>
2021-01-19 17:12:36 +0300

    Added personal page

---
+++ src/translate/personal.ts
@@ -0,0 +1,15 @@
+export const translateText = {
+  english: {
+    pageTitle: 'My Sports Achivments and Learning',
+    text: 'Lorem ipsum dolor sit ament',
+  },
+
+  russian: {
+    pageTitle: 'Мои спортивные достижения',
+    text: 'Lorem ipsum dolor sit ament',
+  },
+  czech: {
+    pageTitle: 'My Sports Achivments and Learning',
+    text: 'Lorem ipsum dolor sit ament',
+  },
+};
