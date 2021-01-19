1e60c238aa6c85a6a3f6705ddbe36cbbfb8a7c4b
mikohan <angara99@gmail.com>
2021-01-19 16:38:12 +0300

    Installed Image Gallery

--- src/components/PersonalityPage.tsx
+++ src/components/PersonalityPage.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { durationPage } from '../config';
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 function PersonalityPage() {
   return (
