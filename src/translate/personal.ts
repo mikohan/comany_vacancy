3226e973636e753f2979a0a986e6d48dcbe0f6fa
mikohan <angara99@gmail.com>
2021-01-20 15:53:40 +0300

    Added personal page

--- src/translate/personal.ts
+++ src/translate/personal.ts
@@ -18,10 +18,24 @@ export const translateText: ITranslateText = {
 
   russian: {
     pageTitle: 'Мои спортивные достижения',
-    text: 'Lorem ipsum dolor sit ament',
+    cheer: {
+      title: 'Cheer Leading',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ducimus
+            magnam quos, explicabo perferendis consequuntur ullam quia inventore
+            dignissimos veritatis itaque. Suscipit qui consectetur eveniet
+            repudiandae, officia neque illum sunt?
+            `,
+    },
   },
   czech: {
     pageTitle: 'My Sports Achivments and Learning',
-    text: 'Lorem ipsum dolor sit ament',
+    cheer: {
+      title: 'Cheer Leading',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ducimus
+            magnam quos, explicabo perferendis consequuntur ullam quia inventore
+            dignissimos veritatis itaque. Suscipit qui consectetur eveniet
+            repudiandae, officia neque illum sunt?
+            `,
+    },
   },
 };
