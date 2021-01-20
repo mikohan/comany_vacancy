29a3ee57e3a588c50281cd5065760d6153a540cf
mikohan <angara99@gmail.com>
2021-01-20 15:51:03 +0300

    Added personal page

--- src/translate/personal.ts
+++ src/translate/personal.ts
@@ -1,16 +1,19 @@
 interface ITranslateText {
   [key: string]: {
-    [key: string]: string;
+    [key: string]: any;
   };
 }
 export const translateText: ITranslateText = {
   english: {
     pageTitle: 'My Sports Achivments and Learning',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ducimus
+    cheer: {
+      title: 'Cheer Leading',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ducimus
             magnam quos, explicabo perferendis consequuntur ullam quia inventore
             dignissimos veritatis itaque. Suscipit qui consectetur eveniet
             repudiandae, officia neque illum sunt?
             `,
+    },
   },
 
   russian: {
