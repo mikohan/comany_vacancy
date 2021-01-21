b2045e65a7a9a15c67350967c90eddbe694d7831
mikohan <angara99@gmail.com>
2021-01-21 10:56:50 +0300

    Added personal page

--- src/components/PersonalityPage.tsx
+++ src/components/PersonalityPage.tsx
@@ -38,7 +38,9 @@ const images = [
 function PersonalityPage() {
   const context = useContext(ThemeContext);
   let { language } = context;
-  const { pageTitle, cheer, freeDiving, ski } = translateText[language];
+  const { pageTitle, cheer, freeDiving, ski, snowboard } = translateText[
+    language
+  ];
   const classes = useStyles();
   return (
     <motion.div
@@ -53,8 +55,8 @@ function PersonalityPage() {
         </Grid>
         <Grid className={classes.verticalGap} item xs={12}>
           <PersonalRow
-            title={ski.title}
-            text={ski.text}
+            title={snowboard.title}
+            text={snowboard.text}
             items={images}
             imageVideo={false}
             vidUrl={[
