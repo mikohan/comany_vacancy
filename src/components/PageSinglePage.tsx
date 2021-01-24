22d4639d2eb1213dfde158564ad9565d5d9c2821
mikohan <angara99@gmail.com>
2021-01-24 16:07:54 +0300

    SinglePageComponent

--- src/components/PageSinglePage.tsx
+++ src/components/PageSinglePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Paper, Grid, Box, Typography } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import ReactPlayer from 'react-player';
+import Divider from '@material-ui/core/Divider';
 
 interface IItem {
   original: string;
@@ -30,6 +31,11 @@ const useStyles = makeStyles((theme: Theme) =>
     textWrapper: {
       padding: theme.spacing(5),
     },
+    paragraph: {
+      fontSize: '1.4rem',
+      marginBottom: theme.spacing(3),
+      textIndent: theme.spacing(4),
+    },
   })
 );
 
@@ -54,32 +60,7 @@ function PageSinglePage() {
           </Box>
         </Grid>
         <Grid item xs={6}>
-          <Box>
-            <Typography variant="body2">
-              Hi, I am Vladimir and in this video I will tell you about my
-              Linux/Unix experience. I have been working in the Linux
-              environment for 12 years. I like Linux for its reliability and a
-              great opportunity to learn new things every day. I worked in
-              different distributions such as Debian, Ubuntu, Fedora, Centos and
-              so on. One day I even built a Gentoo distro just for learning
-              purposes. I can set up and configure pretty much any kind of
-              software in Linux. For scripting sometimes I’m using Shell/Bash
-              sometimes Python. I also have experience with Web servers, like
-              Apache and Nginx and also Python WSGI. Databases servers such as
-              MySQL, MariaDB, PostgreSQL and Elastic search. FTP servers, here
-              are VS FTP, ProFTPD. Network related staff (NFC, SAMBA, Bing9,
-              SSH, etc.). And security related staff: IP tables, Fail to ban,
-              SSl and even more. I also have designed by myself a complex Backup
-              system, using Crone, Python and BASH scripting. During my
-              developer’s career, I have been facing a lot of different
-              difficult tasks. For example, two years ago our company was needed
-              to solve a problem with the VOIP business phone system. And I set
-              up the Asterisk on the Slackware distro from scratch, with no
-              prior knowledge. I tell you it is not for the faint-hearted at
-              all. I like to get to the heart of the matter of things so Linux
-              gives me that opportunity almost every day. Thank you. Bye.
-            </Typography>
-          </Box>
+          <Box></Box>
         </Grid>
       </Grid>
     </Paper>
