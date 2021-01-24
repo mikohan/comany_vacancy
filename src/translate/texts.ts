3cdc284d3f0e8759e3dda431927fa9d293cebce5
mikohan <angara99@gmail.com>
2021-01-24 11:22:19 +0300

    Changed portfolio

--- src/translate/texts.ts
+++ src/translate/texts.ts
@@ -56,7 +56,7 @@ export const translateText: TransText = {
           text: 'Live View',
         },
       },
-      enterpise: {
+      enterprise: {
         title: `Enterprise Management System project (PHP, MySQL, Python, Django, Javascript)`,
         mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
@@ -337,7 +337,7 @@ export const translateText: TransText = {
           text: 'Посмотреть сайт',
         },
       },
-      enterpise: {
+      enterprise: {
         title: `Система Управления Предприятием (PHP, MySQL, Python, Django, Javascript)`,
         mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
@@ -562,6 +562,42 @@ export const translateText: TransText = {
         title: `Venezo Project E-commerce project (Python, Django)`,
         mainText: `Lorem ipsum (zkráceně lipsum) je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů (např. internetových stránek, rozvržení časopisů či všech druhů reklamních materiálů). Lipsum tak pracovně znázorňuje text v ukázkových maketách (tzv. mock-up) předtím, než bude do hotového návrhu vložen smysluplný obsah.
 Pokud by se pro stejný účel použil smysluplný text, bylo by těžké hodnotit pouze vzhled, aniž by se pozorovatel nechal svést ke čtení obsahu. Pokud by byl naopak použit nesmyslný, ale pravidelný text (např. opakování „asdf asdf asdf…“), oko by při posuzování vzhledu bylo vyrušováno pravidelnou strukturou textu, která se od běžného textu liší. Text lorem ipsum na první pohled připomíná běžný text, slova jsou různě dlouhá, frekvence písmen je podobná běžné řeči, interpunkce vypadá přirozeně atd.`,
+        gitHub: {
+          href: 'https://github.com/mikohan/venezoproject',
+          text: 'See code on GitHub',
+        },
+        project: {
+          href: 'https://venezo.ru',
+          text: 'Live View',
+        },
+      },
+      enterprise: {
+        title: `Enterprise Management System project (PHP, MySQL, Python, Django, Javascript)`,
+        mainText: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolor, assumenda. Animi, laudantium perspiciatis. Impedit
+                    vitae nemo, exercitationem tenetur assumenda hic. Pariatur
+                    harum doloribus quas, modi porro voluptate dignissimos eaque
+                    exercitationem? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Nobis consequatur deleniti sunt beatae
+                    placeat dolorem fugit, aut voluptatum architecto mollitia
+                    cumque maxime veritatis alias quo a voluptates eaque
+                    officiis recusandae? Dolorem mollitia incidunt voluptas ut
+                    illum cum quisquam ex, perferendis, dolore ratione nulla
+                    quidem neque expedita! Eligendi obcaecati incidunt, ipsa ut
+                    labore, in minus quia, dolores earum voluptas magnam
+                    similique. Ipsam expedita laboriosam culpa? Ipsum, porro
+                    tempore illo aut tenetur eaque culpa quasi iusto blanditiis
+                    cumque cupiditate incidunt excepturi ducimus soluta
+                    reprehenderit ipsa molestiae ex ad corporis ratione repellat
+                    vel. Dolore perspiciatis quisquam aspernatur, cupiditate
+                    veritatis, placeat fuga omnis pariatur blanditiis iste quis,
+                    recusandae enim neque aliquid deserunt commodi nam doloribus
+                    odio itaque. Pariatur esse, accusantium quia explicabo sed
+                    perferendis! Vero molestiae exercitationem, odit voluptate
+                    molestias expedita facere modi eos blanditiis quia provident
+                    nihil, error nobis voluptatem ipsa iste saepe, obcaecati
+                    quidem itaque incidunt quos dolores! Debitis voluptas dolore
+                    repudiandae.`,
         gitHub: {
           href: 'https://github.com/mikohan/venezoproject',
           text: 'See code on GitHub',
