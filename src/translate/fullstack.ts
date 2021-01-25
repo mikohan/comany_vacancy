a4e250cfa90e03d72db0f7a1975fd7037141bff4
mikohan <angara99@gmail.com>
2021-01-25 14:30:35 +0300

    Linux added

---
+++ src/translate/fullstack.ts
@@ -0,0 +1,48 @@
+interface IItem {
+  [key: string]: {
+    header: string;
+    text: string;
+  };
+}
+export const fullstack: IItem = {
+  english: {
+    header: 'Lunux Admininstration Experience',
+    text: `<p>
+              Hi, I am Vladimir and in this video I will tell you about my
+              Linux/Unix experience. I have been working in the Linux
+              environment for 12 years. I like Linux for its reliability and a
+              great opportunity to learn new things every day. I worked in
+              different distributions such as Debian, Ubuntu, Fedora, Centos and
+              so on. One day I even built a Gentoo distro just for learning
+              purposes.
+            </p>
+            <p> 
+              I can set up and configure pretty much any kind of software in
+              Linux. For scripting sometimes I’m using Shell/Bash sometimes
+              Python. I also have experience with Web servers, like Apache and
+              Nginx and also Python WSGI. Databases servers such as MySQL,
+              MariaDB, PostgreSQL and Elastic search. FTP servers, here are VS
+              FTP, ProFTPD. Network related staff (NFC, SAMBA, Bing9, SSH,
+              etc.). And security related staff: IP tables, Fail to ban, SSl and
+              even more. I also have designed by myself a complex Backup system,
+              using Crone, Python and BASH scripting.
+              </p>
+              <p>During my developer’s
+              career, I have been facing a lot of different difficult tasks. For
+              example, two years ago our company was needed to solve a problem
+              with the VOIP business phone system. And I set up the Asterisk on
+              the Slackware distro from scratch, with no prior knowledge. I tell
+              you it is not for the faint-hearted at all. I like to get to the
+              heart of the matter of things so Linux gives me that opportunity
+              almost every day. Thank you. Bye.
+            </p>`,
+  },
+  russian: {
+    header: 'Опыт Full Stack разработки',
+    text: `jfjfjfjfjjfj`,
+  },
+  czech: {
+    header: 'Full Stack Experience',
+    text: `jfjfjfjfjjfj`,
+  },
+};
