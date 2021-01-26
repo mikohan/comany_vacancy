ff1d6e7e651810b1ede1266f5faafa4dbb3712bb
mikohan <angara99@gmail.com>
2021-01-26 11:30:36 +0300

    Frontend added

--- src/translate/frontend.ts
+++ src/translate/frontend.ts
@@ -6,43 +6,43 @@ interface IItem {
 }
 export const frontend: IItem = {
   english: {
-    header: 'Lunux Admininstration Experience',
+    header: 'Front end Development',
     text: `<p>
               Hi, I am Vladimir and in this video I will tell you about my
-              Linux/Unix experience. I have been working in the Linux
-              environment for 12 years. I like Linux for its reliability and a
-              great opportunity to learn new things every day. I worked in
-              different distributions such as Debian, Ubuntu, Fedora, Centos and
-              so on. One day I even built a Gentoo distro just for learning
-              purposes.
+              Front End experience.
+              </p>
+              <p>
+                I always was a more backend oriented developer but I really loved
+                the idea of Single Page Applications.
+                Because I am a marketologist as well , I  was sceptical about SPAs
+                because search engines didn't recognize it.
             </p>
             <p> 
-              I can set up and configure pretty much any kind of software in
-              Linux. For scripting sometimes I’m using Shell/Bash sometimes
-              Python. I also have experience with Web servers, like Apache and
-              Nginx and also Python WSGI. Databases servers such as MySQL,
-              MariaDB, PostgreSQL and Elastic search. FTP servers, here are VS
-              FTP, ProFTPD. Network related staff (NFC, SAMBA, Bing9, SSH,
-              etc.). And security related staff: IP tables, Fail to ban, SSl and
-              even more. I also have designed by myself a complex Backup system,
-              using Crone, Python and BASH scripting.
-              </p>
-              <p>During my developer’s
-              career, I have been facing a lot of different difficult tasks. For
-              example, two years ago our company was needed to solve a problem
-              with the VOIP business phone system. And I set up the Asterisk on
-              the Slackware distro from scratch, with no prior knowledge. I tell
-              you it is not for the faint-hearted at all. I like to get to the
-              heart of the matter of things so Linux gives me that opportunity
-              almost every day. Thank you. Bye.
+              Yes, SPAs are convenient and fast and look great but there is one big
+              problem: they are not recognized by search engines.
+              It is a very big disadvantage that stopped me from using them.
+            </p>
+            <p>
+              Then as I learned frontend deeply I found out Server Side Rendering Technology.
+              It changed the way I see things.
+              I started learning Javascript and typescript
+              Front End frameworks, Vue, NuxtJs, React and NextJs.
+              I took about 15 courses on Udemy and dived in Frontend Development deeply.
+              I  discover many cutting edge technologies.
+              One of them is microservices. In my opinion microservices is the nearest
+              future of web development. Now i'm learning this technology.
+            </p>
+            <p>
+              Some of my front end projects you can see on this page down below.
+              Thank you for your time. Bye!
             </p>`,
   },
   russian: {
-    header: 'Опыт Full Stack разработки',
+    header: 'Опыт Front End разработки',
     text: `jfjfjfjfjjfj`,
   },
   czech: {
-    header: 'Full Stack Experience',
+    header: 'Front End Experience',
     text: `jfjfjfjfjjfj`,
   },
 };
