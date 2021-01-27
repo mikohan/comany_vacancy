aa5a63ecb00f7cabf9fcef819d323ce44b2621c3
mikohan <angara99@gmail.com>
2021-01-27 18:32:30 +0300

    Add marketing

--- src/translate/marketing.ts
+++ src/translate/marketing.ts
@@ -40,10 +40,49 @@ export const marketing: IItem = {
   },
   russian: {
     header: 'Опыт в Онлайн Маркетинге',
-    text: `jfjfjfjfjjfj`,
+    text: `<p>Я работаю в сфере интернет-маркетинга с 2002 года.
+            Хорошему веб-разработчику абсолютно необходимы маркетинговые знания.
+            Если вы собираетесь зарабатывать деньги на своем веб-проекте, еще до того, как вы начнете его разрабатывать, вы должны сначала разработать маркетинговую стратегию.
+            Маркетинговая стратегия влияет на все в веб-проекте.
+            Какую технологию использовать, какой фреймворк, какая должна быть логика.
+            Сколько времени займет сдача проекта и сколько это будет стоить.
+            Наконец, от этого зависит, будет ли бизнес работать хорошо или умрет.
+            Так что я много узнал и практиковался в маркетинге.</p>
+
+            <p>Я построил автоматизированную систему рекламы
+            Автоматические воронки продаж
+            Системы построения семантических ядер
+            Приложения для обработки естественного языка
+            Аналитическая система эффективности рекламы
+            Автоматические системы для SMM
+            и т.п.</p>
+            <p>Потому что я хорошо разбираюсь в интернет-маркетинге, мои веб-проекты в большинстве случаев удавались.
+            показатели эффективности одного из моих последних проектов вы можете найти ниже.
+Всего хорошего, пока.</p>`,
   },
   czech: {
     header: 'Online Marketing Experience',
-    text: `jfjfjfjfjjfj`,
+    text: `<p>V online marketingu pracuji méně či více od roku 2002.
+            Marketingové znalosti jsou pro dobrého webového vývojáře naprosto nezbytné.
+            </p>
+
+
+            <p>Pokud máte v úmyslu vydělat peníze z vašeho webového projektu, musíte nejprve vytvořit marketingovou strategii.
+            Marketingová strategie ovlivňuje vše ve webovém projektu.
+            Jakou technologii použít, jaký rámec, jaká by měla být logika.
+            Jak dlouho trvá dodání projektu a kolik to bude.
+            Nakonec záleží na tom, zda bude fungovat dobře, nebo zemře.
+            Marketingu jsem se tedy hodně naučil a procvičil.</p>
+
+            <p>Vytvořil jsem automatizovaný reklamní systém
+            Automatizovaný prodej paliv
+            Systémy pro vytváření sémantických jader
+            Aplikace pro zpracování přirozeného jazyka
+            Analytický systém pro efektivní reklamu
+            Automatické systémy pro SMM
+            atd.</p>
+            <p>Protože jsem dobrý v online marketingu, moje webové projekty ve většině případů uspěly.
+            ukazatele výkonu jednoho z mých posledních projektů najdete níže.
+Opatruj se Ahoj.</p>`,
   },
 };
