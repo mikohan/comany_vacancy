e98187f9128c0d717f22e1740f202df0f39b9bf4
mikohan <angara99@gmail.com>
2021-01-29 10:18:19 +0300

    Add personality texts

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -12,6 +12,7 @@ import {
   ListItem,
   ListItemText,
   Typography,
+  Container,
 } from '@material-ui/core';
 import '../tmp.scss';
 import PortfolioSingle from './PortfolioSingle';
@@ -66,185 +67,187 @@ function BackEndPage() {
       initial={{ opacity: 0 }}
       transition={{ duration: durationPage }}
     >
-      <Grid container spacing={3}>
-        <Grid item container xs={12} justify="space-between" spacing={1}>
-          <Grid item xs={4}>
-            <Paper className={classes.fastLinkPapaer} elevation={3}>
-              <Typography variant="body1">{fullStackLinks}</Typography>
-              <List className={classes.listAll}>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#venezo">
-                        {portfolio.venezo.title}
-                      </HashLink>
-                    }
-                    secondary="(Django, Python, Elasticsearch)"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#ducato">
-                        {portfolio.ducato.title}
-                      </HashLink>
-                    }
-                    secondary="(Django, Python, MySQL)"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#suppliers">
-                        {portfolio.suppliers.title}
-                      </HashLink>
-                    }
-                    secondary="(PHP, MySQL, Python, Tensorflow, Keras, Javascript)"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#a77">
-                        {portfolio.enterprise.title}
-                      </HashLink>
-                    }
-                    secondary="(PHP, MySQL, Python, Tensorflow, Keras, Javascript)"
-                  />
-                </ListItem>
-              </List>
-            </Paper>
+      <Container maxWidth="lg" className="containerClass">
+        <Grid container spacing={3}>
+          <Grid item container xs={12} justify="space-between" spacing={1}>
+            <Grid item xs={4}>
+              <Paper className={classes.fastLinkPapaer} elevation={3}>
+                <Typography variant="body1">{fullStackLinks}</Typography>
+                <List className={classes.listAll}>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#venezo">
+                          {portfolio.venezo.title}
+                        </HashLink>
+                      }
+                      secondary="(Django, Python, Elasticsearch)"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#ducato">
+                          {portfolio.ducato.title}
+                        </HashLink>
+                      }
+                      secondary="(Django, Python, MySQL)"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#suppliers">
+                          {portfolio.suppliers.title}
+                        </HashLink>
+                      }
+                      secondary="(PHP, MySQL, Python, Tensorflow, Keras, Javascript)"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#a77">
+                          {portfolio.enterprise.title}
+                        </HashLink>
+                      }
+                      secondary="(PHP, MySQL, Python, Tensorflow, Keras, Javascript)"
+                    />
+                  </ListItem>
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <Paper className={classes.fastLinkPapaer} elevation={3}>
+                <Typography variant="body1">{frontEndLinks}</Typography>
+                <List className={classes.listAll}>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#nuxtChat">
+                          {portfolio.nuxtChat.title}
+                        </HashLink>
+                      }
+                      secondary="(NuxtJS, Socket.io, Express, NodeJS)"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#colors">
+                          {portfolio.colors.title}
+                        </HashLink>
+                      }
+                      secondary="(React, Javascript)"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <HashLink className={classes.hashLink} to="#sheets">
+                          {portfolio.sheets.title}
+                        </HashLink>
+                      }
+                      secondary="(Vanilla Javascript)"
+                    />
+                  </ListItem>
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <Paper className={classes.fastLinkPapaer} elevation={3}>
+                <MiscProject />
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.fastLinkPapaer} elevation={3}>
-              <Typography variant="body1">{frontEndLinks}</Typography>
-              <List className={classes.listAll}>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#nuxtChat">
-                        {portfolio.nuxtChat.title}
-                      </HashLink>
-                    }
-                    secondary="(NuxtJS, Socket.io, Express, NodeJS)"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#colors">
-                        {portfolio.colors.title}
-                      </HashLink>
-                    }
-                    secondary="(React, Javascript)"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary={
-                      <HashLink className={classes.hashLink} to="#sheets">
-                        {portfolio.sheets.title}
-                      </HashLink>
-                    }
-                    secondary="(Vanilla Javascript)"
-                  />
-                </ListItem>
-              </List>
-            </Paper>
+        </Grid>
+        <Grid container spacing={6}>
+          <Grid item container xs={12}>
+            <div id="venezo">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/IHpfcNRXUcU"
+                coverImg={require('../assets/venezo-cover.png')}
+                projectName="venezo"
+              />
+            </div>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.fastLinkPapaer} elevation={3}>
-              <MiscProject />
-            </Paper>
+          <Grid item container xs={12}>
+            <div id="nuxtChat">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/44Nw_QoZDuc"
+                coverImg={require('../assets/nuxt-chat.png')}
+                projectName="nuxtChat"
+                direction
+              />
+            </div>
+          </Grid>
+          <Grid item container xs={12}>
+            <div id="colors">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/zRLzJnDXyi4"
+                coverImg={require('../assets/colors.png')}
+                projectName="colors"
+              />
+            </div>
+          </Grid>
+          <Grid item container xs={12}>
+            <div id="ducato">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/6L9CCT7akuU"
+                coverImg={require('../assets/ducato_cover.png')}
+                projectName="ducato"
+                buttons={true}
+                direction
+              />
+            </div>
+          </Grid>
+          <Grid item container xs={12}>
+            <div id="sheets">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/6L9CCT7akuU"
+                coverImg={require('../assets/sheets.png')}
+                projectName="sheets"
+                buttons={true}
+              />
+            </div>
+          </Grid>
+          <Grid item container xs={12}>
+            <div id="suppliers">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/rVCiNUIsKE0"
+                coverImg={require('../assets/suppliers.png')}
+                projectName="suppliers"
+                buttons={false}
+                direction
+              />
+            </div>
+          </Grid>
+          <Grid item container xs={12}>
+            <div id="a77">
+              <PortfolioSingle
+                vidUrl="https://youtu.be/yy6BsklDG_s"
+                coverImg={require('../assets/a77_2.png')}
+                projectName="a77"
+                buttons={false}
+                direction={false}
+              />
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <ScrollToTop
+              style={{
+                bottom: 100,
+                right: 100,
+              }}
+              showUnder={260}
+            >
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <PresentToAllIcon className={classes.scrollUp} />
+              </IconButton>
+            </ScrollToTop>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid container spacing={6}>
-        <Grid item container xs={12}>
-          <div id="venezo">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/IHpfcNRXUcU"
-              coverImg={require('../assets/venezo-cover.png')}
-              projectName="venezo"
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="nuxtChat">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/44Nw_QoZDuc"
-              coverImg={require('../assets/nuxt-chat.png')}
-              projectName="nuxtChat"
-              direction
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="colors">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/zRLzJnDXyi4"
-              coverImg={require('../assets/colors.png')}
-              projectName="colors"
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="ducato">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/6L9CCT7akuU"
-              coverImg={require('../assets/ducato_cover.png')}
-              projectName="ducato"
-              buttons={true}
-              direction
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="sheets">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/6L9CCT7akuU"
-              coverImg={require('../assets/sheets.png')}
-              projectName="sheets"
-              buttons={true}
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="suppliers">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/rVCiNUIsKE0"
-              coverImg={require('../assets/suppliers.png')}
-              projectName="suppliers"
-              buttons={false}
-              direction
-            />
-          </div>
-        </Grid>
-        <Grid item container xs={12}>
-          <div id="a77">
-            <PortfolioSingle
-              vidUrl="https://youtu.be/yy6BsklDG_s"
-              coverImg={require('../assets/a77_2.png')}
-              projectName="a77"
-              buttons={false}
-              direction={false}
-            />
-          </div>
-        </Grid>
-        <Grid item xs={12}>
-          <ScrollToTop
-            style={{
-              bottom: 100,
-              right: 100,
-            }}
-            showUnder={260}
-          >
-            <IconButton color="primary" aria-label="add to shopping cart">
-              <PresentToAllIcon className={classes.scrollUp} />
-            </IconButton>
-          </ScrollToTop>
-        </Grid>
-      </Grid>
+      </Container>
     </motion.div>
   );
 }
