4be6aedf8cbbaccf062a58db65b0dca99d3a94a8
mikohan <angara99@gmail.com>
2021-01-29 09:48:24 +0300

    Add personality texts

--- src/components/PersonalityPage.tsx
+++ src/components/PersonalityPage.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
 import { durationPage } from '../config';
-import { Grid } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import PersonalRow from './PersonalRowComponent';
 import { translateText } from '../translate/personal';
 import { ThemeContext } from '../context/ThemeContext';
@@ -86,116 +86,117 @@ function PersonalityPage() {
       initial={{ opacity: 0 }}
       transition={{ duration: durationPage }}
     >
-      <Grid container spacing={3}>
-        <Grid item xs={2}></Grid>
-        <Grid item xs={8}>
-          <VideoPageComponent
-            header={vidHeader}
-            text={text}
-            vidUrl="https://youtu.be/W1nPY85dwv0"
-          />
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <VideoPageComponent
+              header={vidHeader}
+              text={text}
+              vidUrl="https://youtu.be/W1nPY85dwv0"
+            />
+          </Grid>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={12}>
+            <h1>{pageTitle}</h1>
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={snowboard.title}
+              text={snowboard.text}
+              items={images}
+              imageVideo={true}
+              vidUrl={[
+                'https://youtu.be/aEYRJWz9pFE',
+                'https://youtu.be/um1PiGKCBi4',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={acrobe.title}
+              text={acrobe.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/bWny3cBI6qk',
+                'https://youtu.be/dR49g_MAStU',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={skateBoard.title}
+              text={skateBoard.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/Ufuslz3-n-Q',
+                'https://youtu.be/r4MRwrWiKDQ',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={ski.title}
+              text={ski.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/Mm-FVcaaNpc',
+                'https://youtu.be/5f7aa0vYoGE',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={freeDiving.title}
+              text={freeDiving.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/c1siyJH_ofk',
+                'https://youtu.be/NF2dpZyx0JY',
+              ]}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <PersonalRow
+              title={cheer.title}
+              text={cheer.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/g2gzxLlMg7Y',
+                'https://youtu.be/NfWgx9b3VjY',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={rollerBlade.title}
+              text={rollerBlade.text}
+              items={images}
+              imageVideo={false}
+              vidUrl={[
+                'https://youtu.be/tL0cQXJ6Pss',
+                'https://youtu.be/rcr4WD5uhg8',
+              ]}
+            />
+          </Grid>
+          <Grid className={classes.verticalGap} item xs={12}>
+            <PersonalRow
+              title={aviation.title}
+              text={aviation.text}
+              items={imagesAviation}
+              imageVideo={true}
+              vidUrl={[
+                'https://youtu.be/4crO3dc2Fb0',
+                'https://youtu.be/4crO3dc2Fb0',
+              ]}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={2}></Grid>
-        <Grid item xs={12}>
-          <h1>{pageTitle}</h1>
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={snowboard.title}
-            text={snowboard.text}
-            items={images}
-            imageVideo={true}
-            vidUrl={[
-              'https://youtu.be/aEYRJWz9pFE',
-              'https://youtu.be/um1PiGKCBi4',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={acrobe.title}
-            text={acrobe.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/bWny3cBI6qk',
-              'https://youtu.be/dR49g_MAStU',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={skateBoard.title}
-            text={skateBoard.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/Ufuslz3-n-Q',
-              'https://youtu.be/r4MRwrWiKDQ',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={ski.title}
-            text={ski.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/Mm-FVcaaNpc',
-              'https://youtu.be/5f7aa0vYoGE',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={freeDiving.title}
-            text={freeDiving.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/c1siyJH_ofk',
-              'https://youtu.be/NF2dpZyx0JY',
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <PersonalRow
-            title={cheer.title}
-            text={cheer.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/g2gzxLlMg7Y',
-              'https://youtu.be/NfWgx9b3VjY',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={rollerBlade.title}
-            text={rollerBlade.text}
-            items={images}
-            imageVideo={false}
-            vidUrl={[
-              'https://youtu.be/tL0cQXJ6Pss',
-              'https://youtu.be/rcr4WD5uhg8',
-            ]}
-          />
-        </Grid>
-        <Grid className={classes.verticalGap} item xs={12}>
-          <PersonalRow
-            title={aviation.title}
-            text={aviation.text}
-            items={imagesAviation}
-            imageVideo={true}
-            vidUrl={[
-              'https://youtu.be/4crO3dc2Fb0',
-              'https://youtu.be/4crO3dc2Fb0',
-            ]}
-          />
-        </Grid>
-      </Grid>
+      </Container>
     </motion.div>
   );
 }
