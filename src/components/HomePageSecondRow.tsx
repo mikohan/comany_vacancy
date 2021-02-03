632ed649643d111059edce6cf2e46074bc25c231
mikohan <angara99@gmail.com>
2021-02-03 11:21:26 +0300

    Working on main page

---
+++ src/components/HomePageSecondRow.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { durationPage } from '../config';
+import ReactPlayer from 'react-player';
+import { useStyles } from '../styles/HomePageStyles';
+import Grid from '@material-ui/core/Grid';
+import { Paper, Box, Typography } from '@material-ui/core';
+
+function HomePageSecondRow() {
+  const classes = useStyles();
+  const vidUrl = 'https://youtu.be/83_P5OBewLw';
+  return (
+    <motion.div
+      exit={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: durationPage }}
+    >
+      <Grid container spacing={2} direction="row" justify="center">
+        <Grid item xs={12}></Grid>
+        <Grid item xs={8}>
+          <Box className="player-wrapper">
+            <ReactPlayer
+              className="react-player"
+              url={vidUrl}
+              controls
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography className={classes.videoSideText} variant="body1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit,
+            libero incidunt id saepe vitae autem consectetur officiis nemo odit,
+            excepturi unde odio quasi, ad recusandae? Nihil magni eum veljit
+            esse?
+          </Typography>
+        </Grid>
+      </Grid>
+    </motion.div>
+  );
+}
+
+export default HomePageSecondRow;
