ab16f6d2a73a2ef0bccfdff8ad1c5a5a8ebedfb7
mikohan <angara99@gmail.com>
2021-02-06 18:28:49 +0300

    Added ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import './App.css';
 
 import PersistentDrawerLeft from './components/MainContent';
 
 import DarkThemeProvider from './context/ThemeContext';
 
+const history = createBrowserHistory();
+
+const trackingId = 'G-LFEBYF29D3'; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+// Initialize google analytics page view tracking
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 function App() {
   return (
-    <DarkThemeProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <PersistentDrawerLeft />
-        </div>
-      </BrowserRouter>
-    </DarkThemeProvider>
+    <Router history={history}>
+      <DarkThemeProvider>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <div className="App">
+            <PersistentDrawerLeft />
+          </div>
+        </BrowserRouter>
+      </DarkThemeProvider>
+    </Router>
   );
 }
 
