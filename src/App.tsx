20edb9174a6b4184673f9c6e846eca17f14d8ae7
mikohan <angara99@gmail.com>
2021-02-07 17:52:31 +0300

    Ga delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import './App.css';
@@ -10,14 +9,6 @@ import PersistentDrawerLeft from './components/MainContent';
 import DarkThemeProvider from './context/ThemeContext';
 
 const history = createBrowserHistory();
-
-const trackingId = 'G-LFEBYF29D3'; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-// Initialize google analytics page view tracking
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 function App() {
   return (
