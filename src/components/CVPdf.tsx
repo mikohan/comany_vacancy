51362e92709e24bef38889c31651fc1510decc51
mikohan <angara99@gmail.com>
2021-02-07 18:33:53 +0300

    Added pdf

--- src/components/CVPdf.tsx
+++ src/components/CVPdf.tsx
@@ -1,4 +1,32 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 import { PDFDownloadLink, Document, Page } from '@react-pdf/renderer';
+interface IProps {
+  image: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paperRow: {
+      width: '100%',
+      padding: theme.spacing(5),
+    },
+    textWrapper: {
+      padding: theme.spacing(5),
+    },
+    paragraph: {
+      fontSize: '1.4rem',
+      marginBottom: theme.spacing(3),
+      textIndent: theme.spacing(4),
+      textAlign: 'left',
+    },
+    fullWidth: {
+      width: '100%',
+    },
+  })
+);
 
 const MyDoc = () => (
   <Document>
@@ -6,12 +34,14 @@ const MyDoc = () => (
   </Document>
 );
 
-const App = () => (
-  <div>
+const CVPdf = () => (
+  <Paper>
     <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
       {({ blob, url, loading, error }) =>
         loading ? 'Loading document...' : 'Download now!'
       }
     </PDFDownloadLink>
-  </div>
+  </Paper>
 );
+
+export default CVPdf;
