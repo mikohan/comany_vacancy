41270927e24c4772e91c0ba34c4c01c9b1b9854c
mikohan <angara99@gmail.com>
2021-02-08 13:18:02 +0300

    Working on pdf

---
+++ src/components/CV/CVComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Paper, Typography } from '@material-ui/core';
+
+export default function CVComponent() {
+  return (
+    <Paper>
+      <Typography variant="body1">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+        sit. Minima, adipisci esse. Quas nulla iure quo temporibus qui sapiente
+        laudantium dicta cumque adipisci vel reprehenderit velit maiores,
+        nesciunt veritatis.
+      </Typography>
+    </Paper>
+  );
+}
