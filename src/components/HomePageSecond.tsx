87c7c917bbf8039150f4ef61a6a2522f76782108
mikohan <angara99@gmail.com>
2021-02-08 11:41:58 +0300

    Working on pdf

--- src/components/HomePageSecond.tsx
+++ src/components/HomePageSecond.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { durationPage } from '../config';
 
 import { useStyles } from '../styles/AboutPageTestStylesl';
-import { Box, Typography, Button, Grid } from '@material-ui/core';
+import { Box, Typography, Button, Grid, Container } from '@material-ui/core';
 import { translateText } from '../translate/texts';
 import { ThemeContext } from '../context/ThemeContext';
 import HomePageSecondRow from './HomePageSecondRow';
@@ -60,7 +60,9 @@ function AboutPageTest() {
           <HomePageSecondRow secondRowText={secondRowText} />
         </Grid>
         <Grid item container xs={12} spacing={0}>
-          <ShowCV />
+          <Container maxWidth="lg">
+            <ShowCV />
+          </Container>
         </Grid>
       </Grid>
     </motion.div>
