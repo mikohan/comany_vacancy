ed8e895d5de2fae337aa2be15670a3f743cf71c8
mikohan <angara99@gmail.com>
2021-02-09 11:29:48 +0300

    Contact page added

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import { Paper, Typography, Container } from '@material-ui/core';
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
 
 export default function Contacts() {
   return (
-    <Paper>
-      <Typography variant="h3">Contacts page</Typography>
-    </Paper>
+    <Container maxWidth="lg">
+      <Paper>
+        <Typography variant="h3">Contacts page</Typography>
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List component="nav" aria-label="secondary mailbox folders">
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+        </List>
+      </Paper>
+    </Container>
   );
 }
