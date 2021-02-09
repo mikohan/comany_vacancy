74019fe46201ffd4cad6528b72faf6233916dcde
mikohan <angara99@gmail.com>
2021-02-09 11:36:55 +0300

    Contact page added

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingTop: theme.spacing(5),
     },
     contactBox: {
-      width: '800px'
+      width: '800px',
       margin: '0 auto',
     },
   })
