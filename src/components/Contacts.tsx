f33a0f6e7a8b898a3c67755481a52e9ee9feb453
mikohan <angara99@gmail.com>
2021-02-09 11:22:16 +0300

    Contact page added

---
+++ src/components/Contacts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page, Typography } from '@material-ui/core';
+
+export default function Contacts() {
+  return (
+    <Paper>
+      <Typography variant="h3">Contacts page</Typography>
+    </Paper>
+  );
+}
