c4a88adc348860df7c22ec6699d92b943c3912e7
mikohan <angara99@gmail.com>
2021-02-27 17:52:09 +0300

    Refactor code

--- src/components/HomePageSecond.tsx
+++ src/components/HomePageSecond.tsx
@@ -76,9 +76,11 @@ function AboutPageTest() {
             </Typography>
           </Box>
         </Grid>
-        <Grid item container xs={12} spacing={0}>
-          <HomePageSecondRow secondRowText={secondRowText} />
-        </Grid>
+        <Hidden mdDown>
+          <Grid item container xs={12} spacing={0}>
+            <HomePageSecondRow secondRowText={secondRowText} />
+          </Grid>
+        </Hidden>
         <Hidden mdDown>
           <Grid item xs={12}>
             <Divider />
