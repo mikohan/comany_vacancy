604fade31779b2d2a6b4b16c81bfe7cac910fad4
mikohan <angara99@gmail.com>
2021-08-24 12:52:08 +0300

    Fixed temp text sales manager

--- src/translate/frontend.ts
+++ src/translate/frontend.ts
@@ -10,35 +10,9 @@ export const frontend: IItem = {
     header: 'Some of Front End Pfojects',
     pageHeader: 'Front End Experience',
     text: `
+    <h1>Вакансия Менеджер по Продажам</h1>
     <p>
-    <h1>HTML Ipsum Presents</h1>
-
-        <p><strong>Pellentesque habitant morbi tristique</strong> senectus
-        et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
-        feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-          <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien
-        ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>,
-        ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci,
-        sagittis tempus lacus enim ac dui.  in turpis pulvinar facilisis. Ut felis.</p>
-
-        <h2>Header Level 2</h2>
-
-        <ol>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ol>
-
-        <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna.
-          Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa.
-          Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus,
-        at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-
-        <h3>Header Level 3</h3>
-
-        <ul>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ul>
+    Comming Soon
     </p>
     `,
   },
@@ -46,71 +20,20 @@ export const frontend: IItem = {
     pageHeader: 'Вакансия Менеджер по Продажам',
     header: 'Несколько Front End проектов',
     text: `
+    <h1>Вакансия Менеджер по Продажам</h1>
     <p>
-    <h1>HTML Ipsum Presents</h1>
-
-        <p><strong>Pellentesque habitant morbi tristique</strong> senectus
-        et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
-        feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-          <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien
-        ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>,
-        ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci,
-        sagittis tempus lacus enim ac dui.  in turpis pulvinar facilisis. Ut felis.</p>
-
-        <h2>Header Level 2</h2>
-
-        <ol>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ol>
-
-        <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna.
-          Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa.
-          Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus,
-        at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-
-        <h3>Header Level 3</h3>
-
-        <ul>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ul>
+    Comming Soon
     </p>
     `,
   },
   czech: {
     pageHeader: 'Front End Experience',
     header: 'Some of Front End Pfojects',
+
     text: `
+    <h1>Вакансия Менеджер по Продажам</h1>
     <p>
-    <h1>HTML Ipsum Presents</h1>
-
-        <p><strong>Pellentesque habitant morbi tristique</strong> senectus
-        et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
-        feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-          <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien
-        ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>,
-        ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci,
-        sagittis tempus lacus enim ac dui.  in turpis pulvinar facilisis. Ut felis.</p>
-
-        <h2>Header Level 2</h2>
-
-        <ol>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ol>
-
-        <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna.
-          Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa.
-          Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus,
-        at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-
-        <h3>Header Level 3</h3>
-
-        <ul>
-           <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-           <li>Aliquam tincidunt mauris eu risus.</li>
-        </ul>
+    Comming Soon
     </p>
     `,
   },
